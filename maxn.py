8b6555e997de8989c5b1e4c45a4519ef6d7e4f89
Gabriel Freundt <gfreundt@gmail.com>
2022-01-14 19:33:38 -0500

    uploads

--- maxn.py
+++ maxn.py
@@ -1,27 +1,25 @@
 import copy
 
 class Grid:
-    def __init__(self, grid_size) -> None:
+    def __init__(self, grid_size, spos) -> None:
         self.size = grid_size
         self.blank_grid = [[0 for i in range(grid_size)] for j in range(grid_size)]
-        self.grid = self.init_grid(copy.deepcopy(self.blank_grid), grid_size)
+        self.grid = self.init_grid(copy.deepcopy(self.blank_grid), grid_size, spos)
         self.nummax = 0
         self.xmax = []
         self.ymax = []
         
-    def init_grid(self, grid, n):
+    def init_grid(self, grid, n, spos):
         iloc = n // 2
-        # 2
-        grid[iloc - 1][iloc-1], grid[iloc + 1][iloc + 1] = 1, 1
-        # 3
-        #grid[iloc - 1][iloc-1], grid[iloc + 1][iloc + 1], grid[iloc + 2][iloc - 3] = 1, 1, 1
+        for pos in spos:
+            grid[iloc - pos[0]][iloc-pos[1]] = 1
         gprint("Starting Position", grid)
         return grid
 
 
 def gprint(title, grid):
     g = len(grid[0])*3 - 1
-    print(f'{" " + title + " ":+^{g}}')
+    print(f'\n{" " + title + " ":+^{g}}')
     for i in grid:
         print(" ".join([f"{j:02d}" for j in i]).replace("00",".."))
 
@@ -72,7 +70,25 @@ def solve(grid, n):
         G.ymax.append(ymax)
         grid[opt[1]][opt[0]] = 0
 
-G = Grid(11)
-solve(G.grid, 2)
-print(f"\nMax N: {G.nummax} -- Max Grid Size: {max(G.xmax)},{max(G.ymax)}")
-gprint("Best Result", reduce_grid(G.final_grid)[0])
+def generate_configs(n):
+    if n == 2:
+        return [[(-1, -1), (1, 1)], [(-1, 0), (1, 0)], [(0, 1), (0, 0)]]
+
+
+def main(n):
+    global G
+    xmax, ymax = [],[]
+    configs = generate_configs(n)
+    for spos in configs:
+        G = Grid(11, spos)
+        solve(G.grid, 2)
+        xmax.append(max(G.xmax))
+        ymax.append(max(G.ymax))
+        print(f"Max N: {G.nummax} -- Max Grid Size: {max(G.xmax)},{max(G.ymax)}")
+        gprint("Best Result", reduce_grid(G.final_grid)[0])
+    accum_results.append({"Q": n, "MaxN": G.nummax, "GridXmax": max(xmax), "GridYmax": max(ymax)})
+
+
+accum_results = []
+main(2)
+print(accum_results)
